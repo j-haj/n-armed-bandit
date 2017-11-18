14f72510132d8483163d7c659068867ff8c04457
Jeff Hajewski <jeff.hajewski@gmail.com>
2017-11-18 16:59:11 -0600

    Adds moving bandits and moving Q-learners

--- src/bandit.py
+++ src/bandit.py
@@ -1,7 +1,7 @@
 import numpy as np
 from collections import namedtuple
 
-Distribution = namedtuple('Distribution', ['mean', 'var']) 
+Distribution = namedtuple('Distribution', ['mean', 'stddev']) 
 
 class Bandit():
     '''n-armed Bandit model
@@ -26,5 +26,44 @@ class Bandit():
             the value sampled from the distribution
         '''
         return np.random.normal(loc=self.distributions[n].mean,
-                                scale=self.distributions[n].var)
+                                scale=self.distributions[n].stddev)
 
+class MovingBandit(Bandit):
+    '''n-armed Bandit model that evolves over time
+
+    There are `n` different normal distributions, similar to the `Bandit` class.
+    However, the mean and variance of these distributions change over time. The
+    `t` parameter determines how many draws it takes for the distributions to
+    change. Each distribution will change after `t` draws from the given
+    distribution.
+    '''
+
+    def __init__(self, n, t):
+        '''Inits Bandit class with `n` arms and evolves each distribution
+        after it has been drawn from `t` times.
+        '''
+        super().__init__(n)
+        self.t = t
+        self.draw_tracker = [0 for i in range(n)]
+
+    def draw(self, n):
+        '''Draws from distribution `n`
+
+        This method also tracks the number of times a distribution has
+        been drawn from and updates the distribution when it has been
+        used `t` times.
+
+        Args:
+            n: distribution to draw from
+
+        Returns:
+            A draw from distribution `n`
+        '''
+        self.draw_tracker[n] += 1
+        val = np.random.normal(loc=self.distributions[n].mean,
+                               scale=self.distributions[n].stddev)
+        if self.draw_tracker[n] % self.t == 0:
+            self.distributions[n] = Distribution(
+                np.random.uniform(low=1, high=10),
+                np.random.uniform(low=1, high=5))
+        return val
--- src/learner.py
+++ src/learner.py
@@ -5,31 +5,14 @@ def softmax(x):
     e_x = np.exp(x - np.max(x))
     return e_x / np.sum(e_x)
 
-class StationaryQLearner():
+class QLearner():
+    '''Base Q-learner class
+    '''
 
-    def __init__(self, n, tau=.1):
-        '''Inits a Stationary Learner
-
-        Args:
-            epsilon: parameter that controls how much searching the learner does
-            n: number of actions available to the learner
-            tau: parameter that controls softmax distribution
-        '''
+    def __init__(self, n):
         self.n = n
-        self.tau = tau
         self.q_table = np.array([0. for i in range(n)])
         self.q_actions = np.array([0 for i in range(n)])
-
-    def generate_action(self):
-        p_vals = softmax(self.q_table/self.tau)
-        action = np.random.choice(self.n, p=p_vals)
-        self.q_actions[action] += 1
-        return action
-
-    def learn(self, action, reward):
-        k = self.q_actions[action]
-        self.q_table[action] = k * self.q_table[action] +  reward
-        self.q_table[action] /= (k + 1)
 
     def average_reward(self):
         '''Returns the average reward
@@ -42,3 +25,54 @@ class StationaryQLearner():
             total_rewards += n_actions
             reward += reward_total
         return reward / total_rewards
+
+class StationaryQLearner(QLearner):
+
+    def __init__(self, n, tau=.1):
+        '''Inits a Stationary Learner
+
+        Args:
+            epsilon: parameter that controls how much searching the learner does
+            n: number of actions available to the learner
+            tau: parameter that controls softmax distribution
+        '''
+        super().__init__(n)
+        self.tau = tau
+
+    def generate_action(self):
+        p_vals = softmax(self.q_table/self.tau)
+        action = np.random.choice(self.n, p=p_vals)
+        self.q_actions[action] += 1
+        return action
+
+    def learn(self, action, reward):
+        k = self.q_actions[action]
+        self.q_table[action] = self.q_table[action] +\
+                               (1./k) * (reward - self.q_table[action])
+
+
+class MovingQLearner(QLearner):
+
+    def __init__(self, n, tau=.1, alpha=.5):
+        '''Inits moving learner
+
+        Args:
+            n: number of actions possible
+            tau: parameter that shapes the action distribution
+        '''
+        super().__init__(n)
+        self.tau = tau
+        self.step_size = alpha
+
+    def generate_action(self):
+        p_vals = softmax(self.q_table/self.tau)
+        action = np.random.choice(self.n, p=p_vals)
+        self.q_actions[action] += 1
+        return action
+
+    def learn(self, action, reward):
+        k = self.q_actions[action]
+        self.q_table[action] = self.q_table[action] +\
+                               self.step_size * (reward - self.q_table[action])
+
+    
