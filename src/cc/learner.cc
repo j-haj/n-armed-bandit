60688274a95f069f0e8c536915f0c3ef71763dae
Jeff Hajewski <jeff.hajewski@gmail.com>
2017-11-19 08:16:37 -0600

    Adds average reward function to learner

--- src/cc/learner.cc
+++ src/cc/learner.cc
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <vector>
 
 #include "learner.h"
@@ -13,3 +14,14 @@ void StationaryQLearner::learn(int action, double reward) const {
   auto k = q_actions_[action];
   q_table_[action] = q_table_[action] + (1. / k) * (reward - q_table_[action]);
 }
+
+double StationaryQLearner::average_reward() const {
+  double prod_sum {0.0};
+  double total_actions {0.0};
+  for (size_t i = 0; i < h_prod.size(); ++i) {
+    prod_sum += q_table_[i] * q_actions_[i];
+    total_actions += q_actions_[i];
+  }
+  return prod_sum / total_actions;
+}
+
